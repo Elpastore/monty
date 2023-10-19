73cf69b0a21f0be0bec04a71210ad575a67b4938
DegreesK0 <kelvinbora@gmail.com>
2023-10-19 10:01:14 +0000

    Added mul function and the exception for comments

--- stack_functions_3.c
+++ stack_functions_3.c
@@ -42,3 +42,22 @@ void sub(stack_t **stack, unsigned int line_number)
 	(*stack)->n = difference;
 }
 
+/**
+ * mul - function that multiplies the top two elements of the stack.
+ * @stack: pointer to the stack
+ * @line_number: the line number of the file
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	int product;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	product = (*stack)->next->n * (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n = product;
+}
+
